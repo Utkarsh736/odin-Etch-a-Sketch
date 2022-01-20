b6ecab6aec3986d143c1e9de7b095827fb0566e1
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-20 19:41:56 +0530

    Css Setup

--- main.js/app.js
+++ main.js/app.js
@@ -1,24 +1,15 @@
 const container = document.getElementById('container');
 
-
-// const grid = document.createElement('div');
-// grid.classList.add('grid');
-
-// for(var i=0; i<16; i++){
-//     var gridBox = document.createElement('div');
-//     gridBox.textContent = i;
-//     grid[i].appendChild(gridBox);
-// };
-
-
-// container.appendChild(grid);
-
 function gridSetup(){
     const grid = document.createElement('div');
+    grid.classList.add('grid');
 
-    for(let i=0; i<16; i++ ){
+    for(let i=1; i<=16; i++ ){
         const gridBox = document.createElement('div');
-        gridBox.textContent = i+1;
+        gridBox.classList.add('gridBox');
+        gridBox.setAttribute('id', i);
+        gridBox.innerHTML = 'Grids';
+        gridBox.textContent = i;
         grid.appendChild(gridBox);
     };
 
