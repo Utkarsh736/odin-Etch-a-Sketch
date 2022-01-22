ddc9cccb235edbc6d69fc9053c3db3a71cf9e29c
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-22 19:11:33 +0530

    Hover event + css added

--- main.js/app.js
+++ main.js/app.js
@@ -5,15 +5,27 @@ function gridSetup(){
     grid.classList.add('grid');
 
     for(let i=1; i<=16; i++ ){
-        const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox');
-        gridBox.setAttribute('id', i);
-        gridBox.innerHTML = 'Grids';
-        gridBox.textContent = i;
-        grid.appendChild(gridBox);
+        const gridSquare = document.createElement('div');
+        gridSquare.classList.add('gridSquare');
+        gridSquare.setAttribute('id', i);
+        gridSquare.textContent = i;
+        grid.appendChild(gridSquare);
     };
 
     container.appendChild(grid);
 };
 
 gridSetup()
+
+const Squares = document.querySelectorAll('.gridSquare');
+
+Squares.forEach((selectSquare)=>{
+    selectSquare.addEventListener('mouseover', ()=>{
+        console.log(selectSquare.id);
+        addClass(selectSquare);
+    });
+});
+
+function addClass(e){
+    e.classList.add('selected');
+}
